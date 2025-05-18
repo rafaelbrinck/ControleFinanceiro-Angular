be80e9c8e75e0de6883aaece9c9f9ea2b1718035
Rafael Brinckmann <rafabrinckmann@gmail.com>
2025-05-18 18:53:42 -0300

    Acrescentando para mostrar nova tabela por tipo de transação

--- src/app/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio.component.ts
@@ -16,7 +16,26 @@ export class RelatorioComponent implements OnInit {
   relatorio = new Relatorio(0, 0, 0);
   listaTransacoes: Transacao[] = [];
 
+  mostrarDetalhes: boolean = false;
+  tipoDetalhe: 'Entrada' | 'Saida' | undefined;
+
   constructor(private transacaoService: TransacaoService) {}
+
+  exibirDetalhes(tipo: 'Entrada' | 'Saida') {
+    this.tipoDetalhe = tipo;
+    this.mostrarDetalhes = true;
+  }
+
+  fecharDetalhes() {
+    this.tipoDetalhe = undefined;
+    this.mostrarDetalhes = false;
+  }
+
+  get transacoesFiltradas(): Transacao[] {
+    return this.listaTransacoes.filter(
+      (transacao) => transacao.tipo === this.tipoDetalhe
+    );
+  }
 
   ngOnInit(): void {
     this.transacaoService.transacoes$.subscribe((transacoes) => {
